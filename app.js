306e198f8b804bac8d98a11fda5aefe637f1e5fd
shawcorey <shaw.corey@gmail.com>
2021-08-10 09:00:17 -0500

    updated pokemon app with GET function

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-fetch("http://localhost:8080/movies", {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    console.log(data);
-});
+//make sure to put all your code in functions
+
+function onClick() {
+    $("#catch-button").click(function (e) {
+        fetch("https://pokeapi.co/api/v2/pokemon", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+        }).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            randomizePokemon(data)
+        });
+    })
+}
+
+function randomizePokemon(pokemon){
+    console.log(pokemon.results)
+    // get data that was passed in from on Click function
+
+//    randomize one pokemon from array (pokemon.results)
+}
+
+onClick();
